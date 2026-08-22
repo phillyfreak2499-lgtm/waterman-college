b3af03df6f6ad3053ddb2d4e159733b70171a207
phillyfreak2499-lgtm <phillyfreak2499@gmail.com>
2026-08-22 13:17:53 -0500

    Tunnel Run: loader fetches 4 engine parts

--- public/games/tunnel-run.js
+++ public/games/tunnel-run.js
@@ -1,8 +1,13 @@
 (function(){
-function load(u){return fetch(u).then(r=>r.text());}
-Promise.all([load("/games/tr-p1.js"),load("/games/tr-p2.js")]).then(function(p){
-  (0,eval)(p[0]+p[1]);
+function load(u){return fetch(u).then(function(r){if(!r.ok)throw new Error(u+" "+r.status);return r.text();});}
+Promise.all([
+  load("/games/tr-part1.js"),
+  load("/games/tr-part2.js"),
+  load("/games/tr-part3.js"),
+  load("/games/tr-part4.js")
+]).then(function(p){
+  (0,eval)(p[0]+p[1]+p[2]+p[3]);
 }).catch(function(e){
-  document.body.innerHTML="<pre style='color:#fff;padding:20px'>Tunnel Run failed: "+e+"</pre>";
+  document.body.innerHTML="<pre style='color:#fff;padding:20px;font:14px monospace'>Tunnel Run failed to load:\n"+e+"</pre>";
 });
 })();
