a8a43a1c18398ab5897fd33e80d3d0ec55312634
phillyfreak2499-lgtm <phillyfreak2499@gmail.com>
2026-08-22 13:13:51 -0500

    Tunnel Run: split-loader for game engine

--- public/games/tunnel-run.js
+++ public/games/tunnel-run.js
@@ -1,17 +1,8 @@
 (function(){
-function load(url){ return fetch(url).then(function(r){ return r.text(); }); }
-Promise.all([load("/games/tr-a.txt"), load("/games/tr-b.txt")]).then(function(parts){
-  var b64 = parts[0] + parts[1];
-  function go(code){ (0,eval)(code); }
-  if (typeof DecompressionStream === "undefined") {
-    document.body.innerHTML = "<pre style='color:#fff;padding:20px'>Please use a modern browser for Tunnel Run.</pre>";
-    return;
-  }
-  var bin = atob(b64);
-  var u = new Uint8Array(bin.length);
-  for (var i = 0; i < bin.length; i++) u[i] = bin.charCodeAt(i);
-  new Response(new Blob([u]).stream().pipeThrough(new DecompressionStream("deflate")))
-    .text().then(go)
-    .catch(function(e){ document.body.innerHTML = "<pre style='color:#fff;padding:20px'>Load error: "+e+"</pre>"; });
+function load(u){return fetch(u).then(r=>r.text());}
+Promise.all([load("/games/tr-p1.js"),load("/games/tr-p2.js")]).then(function(p){
+  (0,eval)(p[0]+p[1]);
+}).catch(function(e){
+  document.body.innerHTML="<pre style='color:#fff;padding:20px'>Tunnel Run failed: "+e+"</pre>";
 });
 })();
